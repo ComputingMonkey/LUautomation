22e7cf4a55a2ed66f322a523b209000f4be5036d
terrace339 <allen.ackroyd1020@gmail.com>
2020-02-03 23:21:33 +0900

    create backup

--- Green.py
+++ Green.py
@@ -17,18 +17,17 @@ def getCompanys(ori_url, num, tag, cla, search_result):
   soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
   blocks = soup.find_all(tag, class_=cla)
   #それぞれのブロックについて企業名を取得
-  if num == 1:
-      companys.append('')
-      companys.append('検索結果' + search_result)
-  companys.append('【' + search_result + '】')
   for block in blocks:
     company = block.string
     companys.append(company)
 
 #num回スクレイピングを繰り返す
-def pagesToCompanys(num):
+def pagesToCompanys(num, search_result):
     for i in range(num):
-      getCompanys(ori_url, num, tag, cla, search_result)
+      if i == 1:
+          companys.append('')
+          companys.append('【' + search_result + '】')
+      getCompanys(ori_url, i, tag, cla, search_result)
       percent = 100 * (i / num)
       print(str(percent) + '％')
       if i == num - 1:
@@ -40,33 +39,29 @@ def pagesToCompanys(num):
 companys = []
 #Greenのスクレイピング
 #ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-search_result = "IT/Web・通信・インターネット系✖️ソフトウェア/パッケージベンダ"
-ori_url = 'https://www.green-japan.com/search_key/01?key=ottqep2gz9fa4c7cwbxq&keyword=&page='
 tag = 'h3'
 cla = 'card-info__detail-area__box__title'
+
+search_result = "IT/Web・通信・インターネット系✖️ソフトウェア/パッケージベンダ"
+ori_url = 'https://www.green-japan.com/search_key/01?key=ottqep2gz9fa4c7cwbxq&keyword=&page='
 last_page = 55
-pagesToCompanys(last_page)
+pagesToCompanys(last_page, search_result)
 
 search_result = "IT/Web・通信・インターネット系✖️モバイル/アプリサービス"
 ori_url = 'https://www.green-japan.com/search_key/01?key=yfbovw38e6359vh5lm8y&keyword=&page='
-tag = 'h3'
-cla = 'card-info__detail-area__box__title'
 last_page = 52
-pagesToCompanys(last_page)
+pagesToCompanys(last_page, search_result)
 
 search_result = "IT/Web・通信・インターネット系✖️IoT/M2M/ロボット"
 ori_url = 'https://www.green-japan.com/search_key/01?key=78u70qo7lx7z8v0ap5az&keyword=&page='
-tag = 'h3'
-cla = 'card-info__detail-area__box__title'
 last_page = 11
-pagesToCompanys(last_page)
+pagesToCompanys(last_page, search_result)
 
 search_result = 'AR/VR/MR'
 ori_url = 'https://www.green-japan.com/search_key/01?key=dkhxk7j2b78767omrvyo&keyword=&page='
-tag = 'h3'
-cla = 'card-info__detail-area__box__title'
 last_page = 4
-pagesToCompanys(last_page)
+pagesToCompanys(last_page, search_result)
+# print(companys)
 
 
 file_name = 'Greenの企業検索結果'
